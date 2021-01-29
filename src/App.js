cae132ca9b99e131ca62d07a74775e6ab9af4384
Hemnani <hemanani543@gmail.com>
2021-01-29 08:32:09 +0530

    created site with create-react-app and created homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-
+import Homepage from './homepage.componets'
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+    <Homepage/>
     </div>
   );
 }
